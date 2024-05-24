87f4ed2de82f3e5daa9eebf84650486bdd27e87f
Siyu Yang <a651685099@163.com>
2024-05-24 13:13:37 +0800

    Add ppu_test, Add ppu tick function.

--- src/ppu.h
+++ src/ppu.h
@@ -1,12 +1,46 @@
 #ifndef NES_EMULATOR_PPU_H_
 #define NES_EMULATOR_PPU_H_
 
+#include <array>
+
+#include "raylib.h"
+
 namespace nes {
 
 class PPU {
  public:
-  explicit PPU();
-  ~PPU();
+  explicit PPU() : scanline_(0), cycle_(0) {}
+  ~PPU() {}
+
+  void Tick() {
+    while (true) {
+      cycle_++;
+
+      if (cycle_ >= kCycleEnd) { // One scanline finish.
+        scanline_++;
+        cycle_ = 0;
+        if (scanline_ >= kScanlineEnd) {
+          scanline_ = 0;
+          cycle_ = 0;
+          break;
+        }
+      }
+
+      // Draw one pixel.
+      std::array<decltype(BLUE), 4> colors = {GRAY, BLACK, LIGHTGRAY, WHITE};
+      int color_idx = GetRandomValue(0, colors.size());
+      const int kPixelSize = 8;
+      DrawRectangle(cycle_ * kPixelSize, scanline_ * kPixelSize, kPixelSize,
+                    kPixelSize, colors[color_idx]);
+    }
+  }
+
+ private:
+  const int kCycleEnd = 341;
+  const int kScanlineEnd = 262;
+
+  int scanline_;
+  int cycle_;
 };
 
 }  // namespace nes
---
+++ test/ppu_test.cc
@@ -0,0 +1,19 @@
+#include "raylib.h"
+
+#include "ppu.h"
+
+int main() {
+  InitWindow(800, 600, "ppu_test");
+  SetTargetFPS(60);
+
+  nes::PPU ppu;
+
+  while (!WindowShouldClose()) {
+    BeginDrawing();
+    ppu.Tick();
+    EndDrawing();
+  }
+
+  CloseWindow();
+  return 0;
+}
